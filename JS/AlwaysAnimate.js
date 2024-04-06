af05bf1ab54f1f79ee26e4c508a595b015cf5ff1
PIX Physix <58399679+StormB1aze@users.noreply.github.com>
2024-04-05 17:03:58 -0700

    Create AlwaysAnimate.js

---
+++ JS/AlwaysAnimate.js
@@ -0,0 +1,16 @@
+export default definePlugin({
+    name: "AlwaysAnimate",
+    description: "Animates anything that can be animated, besides status emojis.",
+    authors: [Devs.FieryFlames],
+
+    patches: [
+        {
+            find: ".canAnimate",
+            all: true,
+            replacement: {
+                match: /\.canAnimate/g,
+                replace: ".canAnimate || true"
+            }
+        }
+    ]
+});
